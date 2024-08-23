6bbed8e5249757da741256bb840f7d538835fcc8
gwonjiseong65@gmail.com <163107918+gwon08@users.noreply.github.com>
2024-08-23 15:58:01 +0900

    네비게이션 바 퍼블리싱

---
+++ src/App.js
@@ -0,0 +1,284 @@
+import Logo from "./assets/instagramlogo-removebg-preview.png";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <body>
+      <div className="left">
+        <Loggo src={Logo} />
+        <Allul>
+          <Ul1>
+            <li>
+              <Btn1>
+                <Home
+                  dataSlot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                  />
+                </Home>
+                <b>홈</b>
+              </Btn1>
+            </li>
+            <li>
+              <Btn2>
+                <Search
+                  dataSlot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
+                  />
+                </Search>
+                검색
+              </Btn2>
+            </li>
+            <li>
+              <Btn3>
+                <Search
+                  dataSlot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
+                  />
+                </Search>
+                탐색 탭
+              </Btn3>
+            </li>
+            <li>
+              <Btn4>
+                <Search
+                  dataSlot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
+                  />
+                </Search>
+                릴스
+              </Btn4>
+            </li>
+            <li>
+              <Btn5>
+                <Search
+                  dataSlot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
+                  />
+                </Search>
+                메시지
+              </Btn5>
+            </li>
+            <li>
+              <Btn6>
+                <Search
+                  dataSlot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
+                  />
+                </Search>
+                알림
+              </Btn6>
+            </li>
+            <li>
+              <Btn7>
+                <Search
+                  dataSlot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
+                  />
+                </Search>
+                만들기
+              </Btn7>
+            </li>
+            <li>
+              <Btn8>
+                <Search
+                  dataSlot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
+                  />
+                </Search>
+                이미지
+              </Btn8>
+            </li>
+          </Ul1>
+          <Ul2>
+            <li>Threads</li>
+            <li>더 보기</li>
+          </Ul2>
+        </Allul>
+      </div>
+    </body>
+  );
+}
+
+const Btn1 = styled.button`
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const Btn2 = styled.button`
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const Btn3 = styled.button`
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const Btn4 = styled.button`
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const Btn5 = styled.button`
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const Btn6 = styled.button`
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const Btn7 = styled.button`
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const Btn8 = styled.button`
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const Homediv = styled.div`
+  height: 220;
+  width: 55;
+`;
+
+const Loggo = styled.img`
+  height: 82px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Home = styled.svg`
+  width: 32px;
+  border: none;
+`;
+
+const Search = styled.svg`
+  width: 32px;
+  border-color: white;
+`;
+
+const Allul = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 50px;
+  border-left: 1px solid black;
+`;
+
+const Ul1 = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 0;
+`;
+
+const Ul2 = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+
+  gap: 23px;
+  padding: 0;
+`;
+
+export default App;
